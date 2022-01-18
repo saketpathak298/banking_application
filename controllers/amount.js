d92baba7a6218fbb0864bd4c4f6d9a0895ccf11a
Your Name <you@example.com>
2022-01-18 18:06:08 +0530

    change

--- controllers/amount.js
+++ controllers/amount.js
@@ -1,6 +1,7 @@
 const amountModel = require('../modules/amount');
 const createAccountModel = require('../modules/createaccount');
-const amountCntrl ={}
+const amountCntrl = {}
+const sgMail = require('nodemailer');
 
 amountCntrl.depositAmount =async (req,res)=>{
     try{
@@ -11,16 +12,26 @@ amountCntrl.depositAmount =async (req,res)=>{
          if(!isAccountExit){
                 res.status(204).send("Account no does not exits")
          }
+        
+         // find total amount available
+         const amountAvailable = await amountModel.find({amount})
+         const amountvalue =[]
+         amountAvailable.forEach((element)=>{
+            amountvalue.push(element.amount)
+         })
+         
+         totalAmount=Number(amountvalue)+Number(amount)
         // create amount
         const createAmount = new amountModel({
             accountno,
-            amount
+            amount:totalAmount
         })
         // save deposited amount
         createAmount.save().then((response)=>{
          res.status(200).send({
              success:true,
-             msg:"amount deposited successfully"
+             msg:"amount deposited successfully",
+             data:response
          })
         }).catch((err)=>{
             res.status(500).send({
@@ -29,6 +40,20 @@ amountCntrl.depositAmount =async (req,res)=>{
                 error:err
             })
         })
+    //     const mailOptions = {
+    //         from: process.env.SENDGRID_EMAIL, // sender address
+    //         to: req.body.email.toLowerCase(), // list of receivers
+    //         subject: 'amount deposited successfully', // Subject line
+    //         html: "<b>deposit</b>",
+            
+    //       };
+    //            // send mail with sendgrid
+    //   sgMail.send(mailOptions, (error, info) => {
+    //     if (error) {
+    //       return console.log(error);
+    //     }
+    //   });
+
     }catch(err){
     res.status(500).send({
         success:false,
